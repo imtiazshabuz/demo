e16678c46b59ad8aa3cf7f09365aa737c5855259
imtiazshabuz <imtiaz.shabuz@gmail.com>
2015-07-26 19:15:02 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<script src="myjsfile.js"></script>
+</head>
+
+<body>
+<form id="myform" method="POST" action=myphp.php>
+<table>
+<tr><td> name: </td>
+<td> <input type="text" name="name" id="name" value=""></td>
+</tr>
+<tr>
+<td> contact </td>
+<td><input typt=text name="contact" id="contact" value=""></td>
+</tr>
+<tr><td><button onclick="myActionhere()">Click here</button> </td></tr>
+</table>
+</form>
+</body>
+</html>
---
+++ myjsfile.js
@@ -0,0 +1,11 @@
+function myActionhere()
+{
+alert("here2");
+name= document.getElementById('name').value;
+alert(name);
+if(name!='')
+{
+document.getElementById('myform').submit();
+}
+else exit;
+}
